b2f848dbea8e20898c1389f3fb26ba9a1a96e8dc
Alex Wambugu <wambugumaigua@DESKTOP-F32N3MJ>
2024-05-29 02:39:01 -0700

    complete lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,16 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def match(self, words):
+        matches = []
+        for potential_anagram in words:
+            if self.is_anagram(potential_anagram):
+                matches.append(potential_anagram)
+        return matches
+
+    def is_anagram(self, potential_anagram):
+        potential_anagram_lower = potential_anagram.lower()
+        return sorted(self.word) == sorted(potential_anagram_lower)
